b1a39328bc82a5139af1792ee69e2b5b48c0c032
Gianluca Colella <36550665+GianluDR@users.noreply.github.com>
2025-11-07 23:37:57 +0100

    added distance interc from player

--- Scripts/interact_area.gd
+++ Scripts/interact_area.gd
@@ -9,6 +9,12 @@ extends Area2D
     "res://scenes/level3.tscn"
 ]
 
+func _physics_process(delta):
+	var player = $"../Player"#get_tree().get_nodes_in_group("Player")[0]
+	var dist = global_position.distance_to(player.global_position)
+	print(dist)
+
+
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "Player":   # oppure usa un gruppo "Player"
 		print("Player ha raggiunto l'area!")
