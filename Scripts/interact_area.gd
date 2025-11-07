2ed8b7cfb07ab581fd9d7b42428000dafbd96688
Gianluca Colella <36550665+GianluDR@users.noreply.github.com>
2025-11-07 23:00:21 +0100

    Update interact_area.gd

--- Scripts/interact_area.gd
+++ Scripts/interact_area.gd
@@ -1,5 +1,19 @@
 extends Area2D
 
+# --- Costanti e variabili di configurazione ---
+@export var interactionType: int
+@export var scene_index: int
+@export var scene_paths = [
+	"res://scenes/test_scene.tscn",
+	"res://scenes/level2.tscn",
+    "res://scenes/level3.tscn"
+]
+
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "Player":   # oppure usa un gruppo "Player"
 		print("Player ha raggiunto l'area!")
+		if(interactionType == 0):
+			print("interazione")
+		else:
+			print("nuovolivello")
+			get_tree().change_scene_to_file(scene_paths[scene_index])
