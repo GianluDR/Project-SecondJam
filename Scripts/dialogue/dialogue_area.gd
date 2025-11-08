64475abc9292fb69cb4d907b38fdfb2e8e4ffb1f
Micello <backup@pwnzerotti.com>
2025-11-08 18:12:39 +0100

    aggiunti contorni bianchi

--- Scripts/dialogue/dialogue_area.gd
+++ Scripts/dialogue/dialogue_area.gd
@@ -58,10 +58,18 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 	if body.is_in_group("player"):
 		player_body_in = true
+		player_body_in = true
+		var parent = get_parent()  # risali al CharacterBody2D
+		parent.get_node("Sprite2D_Highlight").visible = true
+
 		if activate_instant:
 			if only_activate_once and !has_activated_already:
 				_activate_dialogue()
+			
 
 func _on_body_exited(body: Node2D) -> void:
+	player_body_in = false
+	var parent = get_parent()
+	parent.get_node("Sprite2D_Highlight").visible = false
 	if body.is_in_group("player"):
 		player_body_in = false
